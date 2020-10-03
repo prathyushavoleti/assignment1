3534979dc6cae81e10114bf3338dd1bfaf8972cc
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-02 21:06:38 -0700

    Add files via upload

---
+++ giftcard class library.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giftcard
+{
+    public class GiftCard
+    {
+        private long cardno;
+        private bool active;
+        private double balance;
+
+        public long CardNo
+        {
+            get { return cardno; }
+            set { cardno = value; }
+        }
+        public bool Active
+        {
+            get { return active; }
+            set { active = value; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public GiftCard(long Cardno, double balance, bool active)
+
+        {
+            this.cardno = Cardno;
+            this.balance = balance;
+            this.active = Active;
+        }
+        public GiftCard()
+        {
+            this.balance = 0;
+            this.active = true;
+        }
+        public void rechargeCard(int amount)
+        {
+            if (active == true)
+            {
+                balance = balance + amount;
+                Console.WriteLine(balance);
+            }
+            else
+                Console.WriteLine("no amount is added");
+        }
+        public void swipeCard(int amt)
+        {
+            if (active == true && balance > amt)
+            {
+                Console.WriteLine("access granted to swipe the card");
+                balance = balance - amt;
+                Console.WriteLine(balance);
+                
+            }
+            else
+                Console.WriteLine("no access to swipe the card due to insufficient balance");
+        }
+    }
+}
---
+++ giftcard console.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using giftcard;
+
+namespace Giftcard1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GiftCard g = new GiftCard(12345,20000,false);
+            g.rechargeCard(2000);
+            g.swipeCard(400);
+            g.swipeCard(500);
+
+            GiftCard gc = new GiftCard();
+            gc.rechargeCard(3000);
+            gc.swipeCard(500);
+            gc.swipeCard(2600);
+            Console.ReadLine();
+        }
+    }
+}
