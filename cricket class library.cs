b1af061af17463f936d6f802af1207c1aba6a9df
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-02 20:12:05 -0700

    Add files via upload

---
+++ cricket class library.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace absclass
+{
+    public abstract class cricket
+    {
+        private int maxOvers;
+        private int targetScore;
+        private int currentOver;
+        private int currentScore;
+
+        public int getMaxOvers()
+        {
+            return maxOvers;
+        }
+        public int getTargetScore()
+        {
+            return targetScore;
+        }
+        public int CurrentOver
+        {
+            get { return currentOver; }
+            set { currentOver = value; }
+        }
+        public int CurrentScore
+        {
+            get { return currentScore; }
+            set { currentScore = value; }
+        }
+        public cricket(int maxOvers, int targetScore)
+        {
+            this.maxOvers = maxOvers;
+            this.targetScore = targetScore;
+        }
+        public abstract double calcurrentrunrate();
+        public abstract double requiredrunrate();
+
+        public class OneDaycricket : cricket
+        {
+             
+            double currentRunrate, Reqrunrate;
+            public OneDaycricket(int cur_over, int cur_score, int target_score):base(cur_score,cur_over)
+            { 
+                this.currentScore = cur_score;
+                this.currentOver = cur_over;
+                this.targetScore = target_score;
+                this.maxOvers = 50;
+
+            }
+
+            public override double calcurrentrunrate()
+            {
+                currentRunrate = currentScore / currentOver;
+                return currentRunrate;
+            }
+
+            public override double requiredrunrate()
+            {
+                int remainingruns = targetScore - currentScore;
+                int remainingover = maxOvers - currentOver;
+                Reqrunrate = remainingruns / remainingover;
+                return Reqrunrate;
+
+            }
+
+
+
+        }
+
+
+    }
+}
+    
+
+
+                
+
+
+
+
+
+
+
+        
---
+++ cricket console.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using absclass;
+using static absclass.cricket;
+
+namespace abstractclass
+{
+    
+    
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the target runs");
+            int targetRuns = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the current over");
+            int currentOver = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter current score");
+            int currentScore = Convert.ToInt32(Console.ReadLine());
+            OneDaycricket O = new OneDaycricket(currentOver, currentScore, targetRuns);
+
+            double cur_runrate = O.calcurrentrunrate();
+            double req_runrate = O.requiredrunrate();
+            Console.WriteLine("current run rate{0} ", cur_runrate);
+            Console.WriteLine(" required run rate is  {0}", req_runrate);
+            Console.ReadLine();
+
+
+        }
+    }
+}
