95fe828bca4236a4e4a360efa9769b036a89b3da
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-04 23:01:38 -0700

    Add files via upload

---
+++ stackof int.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> mystack = new Stack<int>();
+            mystack.Push(2);
+            mystack.Push(4);
+            mystack.Push(6);
+            mystack.Push(8);
+            mystack.Push(10);
+            mystack.Push(12);
+            mystack.Push(14);
+            mystack.Push(16);
+            mystack.Push(18);
+            mystack.Push(20);
+            foreach(int x in mystack)
+            {
+                Console.WriteLine("elements of mystack are:{0}", x);
+            }
+            Console.WriteLine("================================================================================");
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the peek element of the stack is:" + mystack.Peek());//to find the peek of my stack
+
+            foreach (int x in mystack)
+            {
+                Console.WriteLine("the elements in the stack are{0}",x);
+            }
+      
+            Console.ReadLine();
+        }
+    }
+}
