c1e878ea6cb2f635303096c75da87f1c7e32bfd2
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-05 03:13:24 -0700

    Add files via upload

---
+++ queue0fintegers.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue_of_integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> myqueue = new Queue<int>();
+            myqueue.Enqueue(2);
+            myqueue.Enqueue(4);
+            myqueue.Enqueue(6);
+            myqueue.Enqueue(8);
+            myqueue.Enqueue(10);
+            myqueue.Enqueue(12);
+            myqueue.Enqueue(14);
+            myqueue.Enqueue(16);
+            myqueue.Enqueue(18);
+            myqueue.Enqueue(20);
+            foreach (int x in myqueue)
+            {
+                Console.WriteLine("elements of myqueue are:{0}", x);
+            }
+            Console.WriteLine(myqueue.Contains(8));
+            Console.WriteLine(myqueue.Contains(22));
+            Console.WriteLine("the peek element of the stack is:" + myqueue.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+            Console.WriteLine();
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the peek element of the stack is:" + myqueue.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+            myqueue.Enqueue(22);
+            myqueue.Enqueue(24);
+            Console.WriteLine();
+
+
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the peek element of the stack is:" + myqueue.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+            Console.WriteLine();
+            myqueue.Enqueue(26);
+            myqueue.Enqueue(28);
+            myqueue.Enqueue(30);
+            myqueue.Enqueue(32);
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the peek element of the stack is:" + myqueue.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+            Console.WriteLine();
+            myqueue.Enqueue(34);
+            myqueue.Enqueue(36);
+            myqueue.Enqueue(38);
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the peek element of the stack is:" + myqueue.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+            Console.WriteLine();
+            myqueue.Enqueue(40);
+            myqueue.Enqueue(42);
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the element after applying pop:" + myqueue.Dequeue());
+            Console.WriteLine("the peek element of the stack is:" + myqueue.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", myqueue.Count());
+
+            Console.WriteLine(myqueue.Contains(42));
+            Console.WriteLine(myqueue.Contains(14));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ stack_of_int.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_of_integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> mystack = new Stack<int>();
+            mystack.Push(2);
+            mystack.Push(4);
+            mystack.Push(6);
+            mystack.Push(8);
+            mystack.Push(10);
+            mystack.Push(12);
+            mystack.Push(14);
+            mystack.Push(16);
+            mystack.Push(18);
+            mystack.Push(20);
+            foreach (int x in mystack)
+            {
+                Console.WriteLine("elements of mystack are:{0}", x);
+            }
+            Console.WriteLine(mystack.Contains(8));
+            Console.WriteLine(mystack.Contains(22));
+            Console.WriteLine("the peek element of the stack is:" + mystack.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+            Console.WriteLine();
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the peek element of the stack is:" + mystack.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+            mystack.Push(22);
+            mystack.Push(24);
+            Console.WriteLine();
+
+
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the peek element of the stack is:" + mystack.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+            Console.WriteLine();
+            mystack.Push(26);
+            mystack.Push(28);
+            mystack.Push(30);
+            mystack.Push(32);
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the peek element of the stack is:" + mystack.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+            Console.WriteLine();
+            mystack.Push(34);
+            mystack.Push(36);
+            mystack.Push(38);
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the peek element of the stack is:" + mystack.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+            Console.WriteLine();
+            mystack.Push(40);
+            mystack.Push(42);
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the element after applying pop:" + mystack.Pop());
+            Console.WriteLine("the peek element of the stack is:" + mystack.Peek());//to find the peek of my stack
+            Console.WriteLine("the count of the elements are:{0}", mystack.Count());
+
+            Console.WriteLine(mystack.Contains(2));
+            Console.WriteLine(mystack.Contains(14));
+            Console.ReadLine();
+        }
+    }
+}
