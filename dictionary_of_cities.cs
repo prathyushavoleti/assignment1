aefa15498395221c0ea6d96a49bf694419aeb203
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-05 03:35:22 -0700

    Add files via upload

--- dictionary_of_cities.cs
+++ dictionary_of_cities.cs
@@ -10,86 +10,86 @@ namespace dictionary_of_cities
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> dict = new Dictionary<int, string>();
-            dict.Add(1, "banglore");
-            dict.Add(2, "mumbai");
-            dict.Add(3, "pune");
-            dict.Add(4, "hyderabad");
-            dict.Add(5, "chennai");
-            dict.Add(6, "delhi");
-            dict[7] = "kolkata";
-            dict[8] = "ahmedabad";
-            dict[9] = "noida";
-            dict[10] = "gurgaon";
-            dict[11] = "tiuvanantapuram";
-            foreach (KeyValuePair<int, string> x in dict)
+            Dictionary<string, string> dict = new Dictionary<int, string>();
+            dict.Add("one", "banglore");
+            dict.Add("two", "mumbai");
+            dict.Add("three", "pune");
+            dict.Add("four", "hyderabad");
+            dict.Add("five", "chennai");
+            dict.Add("six", "delhi");
+            dict["seven"] = "kolkata";
+            dict["eight"] = "ahmedabad";
+            dict["nine"] = "noida";
+            dict["ten"] = "gurgaon";
+            dict["eleven"] = "tiuvanantapuram";
+            foreach (KeyValuePair<string, string> x in dict)
             {
                 Console.WriteLine("the elements in the dictionay are:" + x.Key + " " + x.Value);
             }
             Console.WriteLine();
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(1));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(2));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(3));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(4));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(5));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(6));
-            foreach (KeyValuePair<int,string> x in dict)
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("one"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("two"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("three"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("four"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("five"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("six"));
+            foreach (KeyValuePair<string,string> x in dict)
             {
                 Console.WriteLine("the elements in the dictionay are:" + x.Key + " " + x.Value);
             }
             Console.WriteLine("the elements in the dictionay are:" + dict.Count());
             Console.WriteLine();
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(7));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(8));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(9));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(10));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(11));
-            foreach (KeyValuePair<int, string> x in dict)
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("seven"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("eight"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("nine"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("ten"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("eleven"));
+            foreach (KeyValuePair<string, string> x in dict)
             {
                 Console.WriteLine("the elements in the dictionay are:" + x.Key + " " + x.Value);
             }
 Console.WriteLine("the elements in the dictionay are:" + dict.Count());
             //again add the elements to the dictionary
-            dict.Add(1, "banglore");
-            dict.Add(2, "mumbai");
-            dict.Add(3, "pune");
-            dict.Add(4, "hyderabad");
-            dict.Add(5, "chennai");
-            dict.Add(6, "delhi");
+            dict.Add("one", "banglore");
+            dict.Add("two", "mumbai");
+            dict.Add("three", "pune");
+            dict.Add("four", "hyderabad");
+            dict.Add("five", "chennai");
+            dict.Add("six", "delhi");
             Console.WriteLine("the elements in the dictionay are:" + dict.Count());
             
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(1));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(2));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(3));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(4));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("one"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("two"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("three"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("four"));
             
-            foreach (KeyValuePair<int, string> x in dict)
+            foreach (KeyValuePair<string, string> x in dict)
             {
                 Console.WriteLine("the elements in the dictionay are:" + x.Key + " " + x.Value);
             }
             Console.WriteLine("the elements in the dictionay are:" + dict.Count());
             Console.WriteLine();
-            dict[7] = "kolkata";
+            dict["seven"] = "kolkata";
             Console.WriteLine("the elements in the dictionay are:" + dict.Count());
 
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(5));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(6));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(7));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("five"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("six"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("seven"));
             
-            foreach (KeyValuePair<int, string> x in dict)
+            foreach (KeyValuePair<string, string> x in dict)
             {
                 Console.WriteLine("the elements in the dictionay are:" + x.Key + " " + x.Value);
             }
             Console.WriteLine("the elements in the dictionay are:" + dict.Count());
             Console.WriteLine();
-            dict[8] = "ahmedabad";
-            dict[9] = "noida";
+            dict["eight"] = "ahmedabad";
+            dict["nine"] = "noida";
             Console.WriteLine("the elements in the dictionay are:" + dict.Count());
 
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(8));
-            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove(9));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("eight"));
+            Console.WriteLine("elements left in the dictionary after removed" + dict.Remove("nine"));
             
-            foreach (KeyValuePair<int, string> x in dict)
+            foreach (KeyValuePair<string, string> x in dict)
             {
                 Console.WriteLine("the elements in the dictionay are:" + x.Key + " " + x.Value);
             }
