ba2ec2e782d5a1e0300dc80803da6a2a5e78b54d
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-06 07:42:46 -0700

    Add files via upload

---
+++ assignments on 6-10-2020/static_regex.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace static_regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The input string again.
+            string input = "/content/alternate-1.aspx";
+
+            // This calls the static method specified.
+            Console.WriteLine(RegexUtil.MatchKey(input));
+            Console.WriteLine();
+            Console.WriteLine("example -2");
+            string str = "/content/web_design-1.aspx";//input string 
+            //this calls the static method specified
+            Console.WriteLine(regex1.MatchKey(str));
+            Console.ReadLine();
+        }
+    }
+
+    static class RegexUtil
+    {
+        static Regex _regex = new Regex(@"/content/([a-z0-9\-]+)\.aspx$");
+
+        /// This returns the key that is matched within the input.
+
+        static public string MatchKey(string input)
+        {
+            Match match = _regex.Match(input.ToLower());
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    static class regex1
+    {
+        static Regex _regex11 = new Regex(@"/content/([a-z0-9\-]+).aspx$");//returns lowercase only with irrespective of stored data ex: input = ABCd ==> output =abcd
+        //<summary>
+        //this returns the key that is matched within the inputs.
+        //<summary>
+        static public string MatchKey(string str)
+        {
+            Match match = _regex11.Match(str.ToLower());
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ assignments on 6-10-2020/static_regex1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace static_regex1
+{
+    class static_regex1
+    {
+        static void Main(string[] args)
+        {
+            string str = "/content/WEBDesign-1.aspx";//input string 
+            //this calls the static method specified
+            Console.WriteLine(regex1.MatchKey(str));
+            Console.ReadLine();
+        }
+    }
+    static class regex1
+    {
+        static Regex _regex11 = new Regex(@"/content/([a-z0-9\-]+).aspx$");//returns lowercase only with irrespective of stored data ex: input = ABCd ==> output =abcd
+        //<summary>
+        //this returns the key that is matched within the inputs.
+        //<summary>
+        static public string MatchKey(string str)
+        {
+            Match match = _regex11.Match(str.ToLower());
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
