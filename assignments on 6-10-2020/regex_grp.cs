e88f548f6ae7dfde7ddd113ff3e47526e693786b
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-06 01:57:16 -0700

    Add files via upload

---
+++ assignments on 6-10-2020/regex_grp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace regularexp_group
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1: the input string.
+            string input = "/content/alternate-1.aspx";
+
+            // Part 2: call Regex.Match.
+            Match match = Regex.Match(input, @"content/([A-Za-z0-9\-]+)\.aspx$",
+                RegexOptions.IgnoreCase);
+
+            // Part 3: check the Match for Success.
+            if (match.Success)
+            {
+                // Part 4: get the Group value and display it.
+                string key = match.Groups[1].Value;
+                Console.WriteLine(key);
+            }
+            Console.ReadLine();
+        }
+    }
+}
