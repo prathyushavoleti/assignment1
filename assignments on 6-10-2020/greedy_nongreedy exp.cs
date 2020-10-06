b192a99e8d42df44884ed43521acbd8cc8eb56cf
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-06 01:52:33 -0700

    Add files via upload

---
+++ assignments on 6-10-2020/greedy_nongreedy exp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace greedy_nongreedy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "/bird/cat/";
+            //version1:non-greedy
+            var result1 = Regex.Match(str, "^/.*?/");
+            if(result1.Success)
+            {
+                Console.WriteLine("non-greedy:{0}", result1.Value);
+            }
+            //version 2 greedy
+            var result2 = Regex.Match(str, "^/.*/");
+            if (result2.Success)
+            {
+                Console.WriteLine("greedy:{0}", result2.Value);
+            }
+            Console.WriteLine();
+            string s = "/prathyusha/voleti/";
+            //version1:non-greedy
+            var rslt1 = Regex.Match(s, "^/.*?/");
+            if (rslt1.Success)
+            {
+                Console.WriteLine("non-greedy:{0}", rslt1.Value);
+            }
+            //version 2 greedy
+            var rslt2 = Regex.Match(s, "^/.*/");
+            if (rslt2.Success)
+            {
+                Console.WriteLine("greedy:{0}", rslt2.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
