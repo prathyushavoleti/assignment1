bd4d94bbd8a1dda04ad4cbb8dc127e1d5811a7f4
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-07 02:14:53 -0700

    Add files via upload

---
+++ assignments on 7-10-2020/delegate1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate1
+{
+    public delegate void MyDelegate(string msg); // declare a delegate
+    public delegate void MULDelegate(int i,int j);//declaring a delegate for example 2
+
+    
+    class delegate1
+    {
+        // set target method
+        //target method
+        static void MethodA(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void multiplication(int i, int j)
+        {
+            Console.WriteLine("the product of the values is" + (i * j));
+            
+        }
+
+        static void Main(string[] args)
+        {
+            //one way to instantiate a delegate
+            MyDelegate del = new MyDelegate(MethodA);
+            del("amal");
+            // another way to instatiate a delegate
+            MyDelegate del1 = MethodA;
+            del1("prathyusha");
+            // instantiating an element without writing a method ,by setting a lambda expression 
+            MyDelegate del2 = (string msg) => Console.WriteLine(msg);
+            del2("voleti");
+
+            Console.WriteLine();
+            Console.WriteLine("example of delegate");
+            delegate1 d = new delegate1();
+            //invoking a method as a parameter
+            MULDelegate MD = new MULDelegate(multiplication);
+            //invoking the values 
+            MD.Invoke(2, 3);//one way of invoking
+            MD(2, 3);//another way of invoking both returns the same output
+            MULDelegate MD1 = multiplication;
+            MD1(3, 8);
+            MULDelegate MD2 = (int x, int y) => Console.WriteLine(x / y);
+            MD2(6, 2);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignments on 7-10-2020/delegate2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate2
+{
+    public delegate void MyDelegate(string msg);
+    public delegate void dispDelegate(int i);
+    public delegate void disp1Delegate(int i,int j);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del = ClassA.MethodA;
+            InvokeDelegate(del);
+
+            del = ClassB.MethodB;
+            InvokeDelegate(del);
+
+            del = (string msg) => Console.WriteLine("Called lambda expression: " + msg);
+            InvokeDelegate(del);
+            Console.WriteLine();
+            Console.WriteLine("example-2 of delegate2");
+
+            display1 d = new display1();
+            dispDelegate DD = new dispDelegate(d.disp1);
+            DD(3);
+            disp1Delegate DD1 = new disp1Delegate(display2.disp2);
+            DD1(3, 5);
+
+            DD1 = (int i, int j) => Console.WriteLine("the product by using lambda expression" + (i*j));
+            DD1.Invoke(5, 6);
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+
+
+        static void InvokeDelegate(MyDelegate del) // MyDelegate type parameter
+        {
+            del("Hello World");
+        }
+    }
+
+    class ClassA
+    {
+       public static void MethodA(string message)
+        {
+            Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+        }
+    }
+
+    class ClassB
+    {
+        public static void MethodB(string message)
+        {
+            Console.WriteLine("Called ClassB.MethodB() with parameter: " + message);
+        }
+    }
+    class display1
+    {
+        public void disp1(int i)
+        {
+            
+            for (i =0; i <= 5; i++)
+                Console.WriteLine("the natural numbers to be displayed upto 5 is:" + i);
+        }
+    }
+
+    class display2
+    {
+        public static void disp2(int i, int j)
+        {
+            Console.WriteLine("print the numbers" + i + "   " + j);
+        }
+    }
+
+
+}
+
+    
+    
+
