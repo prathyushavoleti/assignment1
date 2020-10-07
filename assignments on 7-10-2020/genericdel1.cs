458835c7c5c50a0ce5a2b74726db3351ab0ff3bc
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-07 04:12:14 -0700

    Add files via upload

---
+++ assignments on 7-10-2020/genericdel1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genericdel1
+{
+   
+            public delegate T mul<T>(T data, T type); // generic delegate
+
+        class genericdel1
+        {
+            static void Main(string[] args)
+            {
+                mul<int> MUL = Mul;
+                Console.WriteLine(MUL(12, 2));
+
+                mul<string> con = Concat;
+                Console.WriteLine(Concat("Amal","Prathyusha"));
+            Console.ReadLine();
+            }
+
+            public static int Mul(int a, int b)
+            {
+                return a*b;
+            }
+
+            public static string Concat(string str1, string str2)
+            {
+                return str1 + str2;
+            }
+        }
+    }
+    
