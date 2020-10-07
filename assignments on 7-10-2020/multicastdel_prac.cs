d08496baf08e55f1810c9b8bf923f74f9024e517
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-07 08:27:54 -0700

    Add files via upload

---
+++ assignments on 7-10-2020/multicastdel_prac.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multicastdel_prac
+{
+    public delegate void RectangleDelete(int Width, int Height);
+    public class multicastdel_prac
+    {
+        public void Area(int Width, int Height)
+        {
+            Console.WriteLine("Area is {0}", (Width * Height));
+        }
+        public void Perimeter(int Width, int Height)
+        {
+            Console.WriteLine("Perimeter is {0}", (2 * (Width + Height)));
+        }
+        static void Main(string[] args)
+        {
+            multicastdel_prac rect = new multicastdel_prac();
+            RectangleDelete rectDelegate = new RectangleDelete(rect.Area);
+
+            // In this example rectDelegate is a multicast delegate. 
+            // we use += operator to combine delegates together and -= operator to remove.
+            rectDelegate += rect.Perimeter;
+            rectDelegate(5, 10);
+
+            Console.WriteLine();
+            rectDelegate(10,5);
+            Console.WriteLine();
+            //Removing a method from delegate object
+            rectDelegate -= rect.Perimeter;
+            rectDelegate(10, 5);
+            Console.ReadLine();
+        }
+    }
+    
+}
