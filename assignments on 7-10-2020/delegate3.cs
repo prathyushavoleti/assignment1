dd01ec7b295c59b3f0c8191eda969026f5521a0e
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-07 03:56:27 -0700

    Add files via upload

---
+++ assignments on 7-10-2020/delegate3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate3
+{
+    public delegate void MyDelegate(string msg); //declaring a delegate
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del1 = ClassA.MethodA;
+            MyDelegate del2 = ClassB.MethodB;
+
+            MyDelegate del = del1 + del2; // combines del1 + del2
+            del("Hello World");
+
+            MyDelegate del3 = (string msg) => Console.WriteLine("Called lambda expression: " + msg);
+            del += del3; // combines del1 + del2 + del3
+            del("Hello World");
+
+            del = del - del2; // removes del2
+            del("Hello World");
+
+            del -= del1;// removes del1
+           del("Hello World");
+
+           
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class ClassA
+    {
+       public static void MethodA(string message)
+        {
+            Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+        }
+    }
+
+    class ClassB
+    {
+       public static void MethodB(string message)
+        {
+            Console.WriteLine("Called ClassB.MethodB() with parameter: " + message);
+        }
+    }
+
+   
+    
+}
---
+++ assignments on 7-10-2020/multicastdel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multicastdel
+{
+    public delegate int MyDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del1 = ClassA.MethodA;
+            MyDelegate del2 = ClassB.MethodB;
+
+            MyDelegate del = del1 + del2;
+            Console.WriteLine(del());
+            Console.ReadKey();
+        }
+    }
+    class ClassA
+    {
+        public static int MethodA()
+        {
+            return 100;
+        }
+    }
+
+    class ClassB
+    {
+        public static int MethodB()
+        {
+            return 200;
+        }
+    }
+   
+}
