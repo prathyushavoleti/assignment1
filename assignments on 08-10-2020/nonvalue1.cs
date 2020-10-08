e291b03830955c41a2a7172592fd6e9d71157518
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-08 00:53:08 -0700

    Add files via upload

---
+++ assignments on 08-10-2020/nonvalue1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymous_methodnonvalue
+{
+    public delegate void numsDelegate(int i, int j);
+    public delegate void wishDelegate(string msg);
+    class nonvalue
+    {
+        static void Main(string[] args)
+        {
+            numsDelegate ND = new numsDelegate(addnums);
+            numsDelegate ND1 = new numsDelegate(mulnums);
+            wishDelegate WD = new wishDelegate(wishes);
+            wishDelegate WD1 = new wishDelegate(greetings);
+           
+            ND.Invoke(2, 3);
+            ND1.Invoke(5, 6);
+            WD("hi");
+            WD1("birth");
+            Console.ReadLine();
+
+        }
+        public static void addnums(int i, int j)
+        {
+            Console.WriteLine(i + j);
+        }
+        public static void mulnums(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public static void wishes(string msg)
+        {
+            Console.WriteLine("hello good morning");
+        }
+        public static void greetings(string s)
+        {
+            Console.WriteLine("happy"+" "+s+"day");
+        }
+
+
+    }
+}
---
+++ assignments on 08-10-2020/nonvalue2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymous_method2
+{
+    class nonvalue2
+    {
+        public delegate void numDelegate(int i, int j);
+        public delegate void wishDelegate(string msg);
+        static void Main(string[] args)
+        {
+            numDelegate ND = delegate (int i, int j)
+              {
+                  Console.WriteLine(i + j);
+              };
+            ND(2, 3);
+            numDelegate ND1 = delegate (int i, int j)
+            {
+                Console.WriteLine(i * j);
+            };
+            ND1(5,6);
+            wishDelegate WD = delegate (string msg)
+            {
+                Console.WriteLine("hello" + " " + msg + " " + "morning");
+            };
+            WD.Invoke("good");
+            wishDelegate WD1 = delegate (string msg)
+            {
+                Console.WriteLine("happy" + " " + msg + "day");
+            };
+            WD1.Invoke("birthday");
+            Console.ReadLine();
+        }
+    }
+}
