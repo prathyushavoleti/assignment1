64d2ca142162c003a93c837931fdd9209acf562e
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-08 01:27:03 -0700

    Add files via upload

---
+++ assignments on 08-10-2020/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace anonymous_assignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = new Button();//reference to the button
+            b.Text = "click here";//text implemented on the button appearance
+            b.Size = new Size(100, 50);//size maintained for the button
+            b.Location = new Point(650, 300);//pointer location of button
+            this.Controls.Add(b);//controlling mechanism of button shld go center
+            b.Click += new EventHandler(button1_Click1);
+        }
+        private void button1_Click1(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world");
+        }
+
