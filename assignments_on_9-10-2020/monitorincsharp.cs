9b4714f2e8e2b666c029469dd9217fb4256cc2c5
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-09 11:10:43 -0700

    Add files via upload

---
+++ assignments_on_9-10-2020/monitorincsharp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace monitorincsharp
+{
+    class monitorincsharp
+    {
+        static readonly object obj = new object();         
+            public static void numbers()
+            {
+            Monitor.Enter(obj);   
+                  
+                    for (int i = 0; i==5; i++)   
+                    {   
+                        Thread.Sleep(5000);   
+                        Console.Write(i + ",");   
+                    }   
+                    Console.WriteLine();   
+                 
+                 
+                    Monitor.Exit(obj);   
+                  
+            }   
+       
+            static void test()
+           {   
+                   
+                lock (obj)   
+                {   
+                    Thread.Sleep(3000);   
+                   Console.WriteLine(Environment.TickCount);   
+                }   
+          }   
+       
+          static void Main(string[] args)
+            {   
+       
+                   
+                Thread[] thread = new Thread[3];   
+                for (int i = 0; i< 3; i++)   
+                {   
+                    thread[i] = new Thread(new ThreadStart(numbers));   
+                    thread[i].Name = "Child " + i;   
+                }   
+                foreach (Thread t in thread)   
+                    t.Start();   
+       
+                Console.ReadLine();   
+            }   
+       }   
+    } 
+
+        
