96d86d2c780c58068195c8f42ae8d39facad36ad
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-10-09 02:07:32 -0700

    Add files via upload

--- assignments_on_9-10-2020/multithreading.cs
+++ assignments_on_9-10-2020/multithreading.cs
@@ -37,28 +37,28 @@ namespace multithreading_nonstaticreturn
             }
             return "thread3 started";
         }
-        public double Thread4(double length, double width)
-        {
-            Console.WriteLine("area of a rectangle is");
-            return length * width;
-        }
+       
         static void Main(string[] args)
         {
             multithreading m = new multithreading();
-            string s=null;
-            int x=0;
-            string v=null;
-            double d=0;
+            
+
             //one main thread and 4 child threads
-            Thread T1 = new Thread(()=> { s = m.Thread1("prathyusha"); });
-            Console.WriteLine(s);//create the child threads wrt to methods involved
-            Thread T2 = new Thread(() => { x = m.Thread2(5); });
-            Console.WriteLine(x);
-            Thread T3 = new Thread(() => { v = m.Thread3("thread concept"); });
-            Console.WriteLine(v);
-            Thread T4 = new Thread(() => { d = m.Thread4(12.34,34.54); });
-            Console.WriteLine(d);
-            T1.Start(); T2.Start(); T3.Start(); T4.Start();
+            Thread T1 = new Thread(delegate ()
+            {
+                Console.WriteLine(m.Thread1("prathyusha"));
+            });
+//create the child threads wrt to methods involved
+            Thread T2 = new Thread(delegate ()
+            {
+                Console.WriteLine(m.Thread2(5));
+            });
+            Thread T3 = new Thread(delegate ()
+            {
+                Console.WriteLine(m.Thread3("thread concept"));
+            });
+            
+            T1.Start(); T2.Start(); T3.Start(); 
             Console.ReadLine();
             
 
