f5072d7deff047f68699b22d50a19d32148a3c79
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-11-06 09:09:23 -0800

    Add files via upload

---
+++ angularpractice/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Welcome to Angular';
+    subtitle = '.NET Core + Angular CLI v7 + Bootstrap & FontAwesome + Swagger Template';
+    title1 = 'Programming is Fun';
+    subtitle2 = 'This is crazy!!!!!!!!!!';
+}
