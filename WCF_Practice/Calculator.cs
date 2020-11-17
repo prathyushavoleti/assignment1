5819b04ff519c809ec859a5003716f5d6aaa5895
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-11-17 07:43:13 -0800

    Add files via upload

---
+++ WCF_Practice/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Calculator" in both code and config file together.
+    public class Calculator : ICalculator
+    {
+        public int sum(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        public int sub(int num1, int num2)
+        {
+            if (num1 > num2)
+                return num1 - num2;
+            else
+                return 0;
+        }
+        public int mul(int num1, int num2)
+        {
+            return num1*num2;
+        }
+        public int div(int num1, int num2)
+        {
+            if (num2 != 0)
+                return num1 / num2;
+            else
+                return 0;
+        }
+        public int mod(int num1,int num2)
+        {
+            return num1 % num2;
+        }
+        public int even_odd(int num1)
+        {
+            if (num1 % 2 == 0)
+                return num1;
+            else
+                return 0;
+        }
+        public void natural_numbers(int num1)
+        {
+            if (num1==10)
+
+                for (int i=0;i<=num1;i++)
+                Console.WriteLine(i);
+           
+        }
+
+
+
+    }
+}
---
+++ WCF_Practice/ICalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculator" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        //void DoWork();
+        int sum(int num1, int num2);
+        [OperationContract]
+        //void DoWork();
+        int sub(int num1, int num2);
+        [OperationContract]
+        //void DoWork();
+        int mul(int num1, int num2);
+        [OperationContract]
+        //void DoWork();
+        int div(int num1, int num2);
+        [OperationContract]
+        int mod(int num1, int num2);
+        [OperationContract]
+        int even_odd(int num1);
+        [OperationContract]
+        string display(string str1, string str2);
+       
+
+    }
+
+}
+[DataContract]
+public class compositetype
+{
+    bool boolvalue = true;
+    string stringvalue = "hello world";
+    [DataMember]
+    public bool Boolvalue
+    {
+        get { return boolvalue; }
+        set { boolvalue = value; }
+    }
+    public string stringValue
+    {
+        get { return stringvalue; }
+        set { stringvalue = value; }
+    }
+}
