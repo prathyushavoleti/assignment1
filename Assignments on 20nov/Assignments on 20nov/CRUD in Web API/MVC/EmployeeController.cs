f0323ac5104df8d13aac57e7aecbf78e168373ac
Prathyusha voleti <72141206+prathyushavoleti@users.noreply.github.com>
2020-11-20 09:17:07 -0800

    Add files via upload

---
+++ Assignments on 20nov/Assignments on 20nov/CRUD in Web API/MVC/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBMVC.Models;
+using System.Net.Http;
+namespace WEBMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<EmployeeModel> emplist;
+            HttpResponseMessage response = GlobalVariables.webhttpclient.GetAsync("Employee").Result;
+            emplist = response.Content.ReadAsAsync<IEnumerable<EmployeeModel>>().Result;
+            return View(emplist);
+        }
+        public ActionResult AddOrEdit(int id=0)
+        {
+            if(id==0)
+            return View(new EmployeeModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.webhttpclient.GetAsync("Employee"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<EmployeeModel>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(EmployeeModel emp)
+        {
+            if (emp.EmployeeID == 0)
+            {
+            HttpResponseMessage response = GlobalVariables.webhttpclient.PostAsJsonAsync("Employee", emp).Result;
+            TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.webhttpclient.PutAsJsonAsync("Employee"+emp.EmployeeID,emp).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+
+            }
+            return RedirectToAction("Index");
+           
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.webhttpclient.DeleteAsync("Employee/"+id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Assignments on 20nov/Assignments on 20nov/CRUD in Web API/MVC/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEBMVC.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<int> Salary { get; set; }
+
+    }
+}
